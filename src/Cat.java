686f7c986902fff220e1c79da276149beec958a6
Anggunramadhani <anggung.ramadhani@gmail.com>
2024-03-04 14:49:10 +0700

    first commit

---
+++ src/Cat.java
@@ -0,0 +1,38 @@
+public class Cat {
+     String Nama = "Molly";
+     String Makananfavorit = "Wiskas";
+     String warna = "Putih";
+     int umur = 5 ;
+     int berattubuh = 20;
+
+     public void Nama() {
+         System.out.println("Nama kucing ini"+ this.Nama );
+     }
+
+     public void Makananfavorit() {
+
+         System.out.println(this.Nama+"Makanan favoritnya" + this.Makananfavorit);
+     }
+
+     public void warna() {
+
+         System.out.println(this.Nama+"berwarna" + this.warna);
+     }
+
+     public void umur(){
+         System.out.println(this.Nama+"berumur"+this.umur);
+     }
+
+     public void berattubuh(){
+         System.out.println(this.Nama+"berat tubuhnya"+this.berattubuh);
+     }
+        // System.out.println("Berat tubuh: "+berattubuh);
+
+
+
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args) {
+        Cat kucing=new Cat();
+        kucing.Nama();
+        kucing.Makananfavorit();
+        kucing.warna();
+        kucing.umur();
+        kucing.berattubuh();
+    }
+
+
+
+
+
+
+
+
+}
